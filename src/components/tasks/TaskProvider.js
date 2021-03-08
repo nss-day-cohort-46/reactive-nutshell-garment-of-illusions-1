93bbb0745381aba95602efef6b3ad03ab61e068f
jakefroeb <jake.herman.froeb@gmail.com>
2021-03-08 16:57:28 -0600

    made a button that will change the completed key value pair to true

--- src/components/tasks/TaskProvider.js
+++ src/components/tasks/TaskProvider.js
@@ -20,9 +20,21 @@ export const TaskProvider = (props) => {
         .then(res => res.json())
         .then(setTasks)
     }
+    const completeTask = (taskId) => {
+        return fetch(`http://localhost:8088/tasks/${taskId}`,{
+            method:"PATCH",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body: JSON.stringify({
+                completed:true
+            })
+        })
+        .then(getTasks)
+    }
 
     return(
-        <TaskContext.Provider value= {{tasks, getTasks, addTask, setTasks}}>
+        <TaskContext.Provider value= {{tasks, getTasks, addTask, setTasks, completeTask}}>
             {props.children}
         </TaskContext.Provider>
     )
