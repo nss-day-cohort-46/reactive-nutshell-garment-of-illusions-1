8b31876aaf1a482bdddf25fa20fca1560c71d3e3
jakefroeb <jake.herman.froeb@gmail.com>
2021-03-10 14:46:23 -0600

    weather now shows for the date of the event if the date of event is within a week

--- src/components/weather/WeatherProvider.js
+++ src/components/weather/WeatherProvider.js
@@ -8,8 +8,13 @@ export const WeatherProvider = (props) => {
         return fetch(`http://api.openweathermap.org/data/2.5/weather?q=Nashville&appid=${weatherKey}`)
         .then(res => res.json())
     }
-    return (
-        <WeatherContext.Provider value={{getTodaysWeather}}>
+    const getEventWeather = (dayDiff) => {
+        return fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=36.1627&lon=-86.7816&exclude=minutely,hourly,alerts&appid=${weatherKey}`)
+        .then(res => res.json())
+        .then(weather => weather.daily[dayDiff])
+    }
+return (
+        <WeatherContext.Provider value={{getTodaysWeather, getEventWeather}}>
             {props.children}
         </WeatherContext.Provider>
     
